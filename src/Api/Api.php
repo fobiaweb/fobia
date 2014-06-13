74b0cb207fb8475f56c60ce9bc826f5c5d8d6e10
Dmitriy Tyurin <fobia3d@gmail.com>
2014-06-13 21:38:36 +0400

    api

---
+++ src/Api/Api.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Api class  - Api.php file
+ *
+ * @author     Dmitriy Tyurin <fobia3d@gmail.com>
+ * @copyright  Copyright (c) 2014 Dmitriy Tyurin
+ */
+
+namespace Api;
+
+/**
+ * Api class
+ *
+ * @package   Api
+ */
+class Api
+{
+    public function execute()
+    {
+        
+    }
+}
