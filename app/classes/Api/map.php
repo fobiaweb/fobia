c73de1f815dcfc2f2609475a5a4b7e1e0e0c1f34
Dmitriy Tyurin <fobia3d@gmail.com>
2014-08-01 11:20:29 +0400

    map api

---
+++ app/classes/Api/map.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'auth.login' => array('object', '\\Api_Auth_Login'),
+    'auth.logout' => array('object', '\\Api_Auth_Logout'),
+    'utils.getServerTime' => array('object', '\\Api_Utils_GetServerTime')
+);
